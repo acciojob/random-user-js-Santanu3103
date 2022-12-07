81ba78897e26331ca5f6ab22a8a568b05ce7a6b5
santunu <santanuroy253396@gmail.com>
2022-12-08 01:11:34 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-//your code here
+const url ="https://randomuser.me/api/";
+
+
+
+async function getData(url) {
+
+    // TO FETCH DATA
+    const response = await fetch(url);
+  let  data = await response.json();
+
+    console.log(data.results[0]);
+
+        document.getElementById('name').innerHTML =  `${data.results[0].name.first} ${data.results[0].name.last}`;
+      
+      const image = document.getElementsByTagName("img")[0];
+      
+        image.src = `${data.results[0].picture.large}`;
+
+        about = document.getElementsByClassName('about')[0];
+        heading2 = document.getElementsByClassName('detail1')[0];
+        heading3 = document.getElementsByClassName('detail2')[0];
+        
+        
+        heading2.innerHTML =`<b>${data.results[0].dob.age}</b>` ;
+  
+        heading3.innerHTML = `<br>${data.results[0].email}`+ "<br>" +`${data.results[0].phone}`;
+        
+      document.getElementById('getUser').onclick = ()=>{
+
+        heading2.innerHTML =`<b>${data.results[0].dob.age}</b>` ;     
+        heading3.innerHTML = `<br>${data.results[0].email}`+ "<br>" +`${data.results[0].phone}`;
+
+      }
+        document.getElementsByClassName("age")[0].onclick = ()=>{
+
+          heading2.innerHTML =`<b>${data.results[0].dob.age}</b>` ; 
+          heading3.innerHTML = ``;
+          
+      }
+      
+      document.getElementsByClassName("email")[0].onclick = ()=>{
+
+        heading2.innerHTML =``;     
+        heading3.innerHTML = `${data.results[0].email}`;
+    
+    }
+    
+    document.getElementsByClassName("phn_no")[0].onclick = ()=>{
+
+      heading2.innerHTML =`` ;      
+      heading3.innerHTML = `${data.results[0].phone}`;
+  
+  }
+  
+    
+
+}
+
+     
+document.getElementById('getUser').addEventListener('click', ()=>{
+      
+      getData(url);
+
+})
+ 
+  
